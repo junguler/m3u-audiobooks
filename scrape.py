fb06dc6793238f8bf24b18d1064ae5f9c1ae7004
hossein s. borhani <junguler@yahoo.com>
2022-11-16 01:46:40 +0330

    add comments and include insipiration page

--- scrape.py
+++ scrape.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
+# enable passing url from the command line
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--url", help="the web page to scrape from", default='https://www.google.com/')
 
 args = parser.parse_args()
 
+# make firefox headless
 options = Options()
 options.headless = True
 
@@ -17,20 +19,26 @@ driver = webdriver.Firefox(options=options)
 
 driver.maximize_window()
 
+# get url to scrape
 driver.get(args.url)
 
+# amount of seconds to wait before each scroll
 SCROLL_PAUSE_TIME = 5
 
 last_height = driver.execute_script("return document.body.scrollHeight")
 
+# number of total scrolls, usefull for infinite scroll websites
 scroll_limit = 100
 
 count = 0
 while True and count < scroll_limit:
+    # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+    # Wait to load page
     sleep(SCROLL_PAUSE_TIME)
 
+    # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break
@@ -46,3 +54,5 @@ for a in soup.find_all('a', href=True):
     print(a['href'])
 
 driver.close()
+
+# script was adapted from this gist https://gist.github.com/wkarney/11e5f1beeb85f7670f1a077115c681e2
